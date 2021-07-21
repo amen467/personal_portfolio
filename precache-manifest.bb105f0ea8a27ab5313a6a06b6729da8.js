f3e4d3d72a6451c6b2ed2579b2d8691b5d90ff53
amen467 <amen467@gmail.com>
2021-07-21 11:39:30 -0400

    Updates

---
+++ precache-manifest.bb105f0ea8a27ab5313a6a06b6729da8.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06bdd365ed8ea169215dd66b6e765167",
+    "url": "/personal_portfolio/index.html"
+  },
+  {
+    "revision": "886dca37b1c358cffbe6",
+    "url": "/personal_portfolio/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "91d8ed4894f116eb95bf",
+    "url": "/personal_portfolio/static/css/4.ea3dbbf2.chunk.css"
+  },
+  {
+    "revision": "64141919929e9fa57e49",
+    "url": "/personal_portfolio/static/css/main.10b4d4d6.chunk.css"
+  },
+  {
+    "revision": "169cd515b3207b47dff9",
+    "url": "/personal_portfolio/static/js/2.c465168b.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/personal_portfolio/static/js/2.c465168b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "886dca37b1c358cffbe6",
+    "url": "/personal_portfolio/static/js/3.4fcbe37c.chunk.js"
+  },
+  {
+    "revision": "91d8ed4894f116eb95bf",
+    "url": "/personal_portfolio/static/js/4.91b6d496.chunk.js"
+  },
+  {
+    "revision": "64141919929e9fa57e49",
+    "url": "/personal_portfolio/static/js/main.00ea8bef.chunk.js"
+  },
+  {
+    "revision": "4e079ce34cee74649cbb",
+    "url": "/personal_portfolio/static/js/runtime-main.b18ea793.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/personal_portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/personal_portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/personal_portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/personal_portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "53b2ad717fb383dc83d8ec5662e35fa7",
+    "url": "/personal_portfolio/static/media/cpLogo.53b2ad71.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/personal_portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "f000ce303006b703f6c77f18648cf59e",
+    "url": "/personal_portfolio/static/media/me.f000ce30.jpg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/personal_portfolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/personal_portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/personal_portfolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/personal_portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/personal_portfolio/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "53015aad4cf940e4d341c556ac2d8014",
+    "url": "/personal_portfolio/static/media/vodafoneLogo.53015aad.png"
+  }
+]);
